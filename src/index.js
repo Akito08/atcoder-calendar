723447e53a7f32d3fa0e5494286661b2f14faac0
Akito08 <sza9ki8to7@gmail.com>
2023-11-09 15:58:30 +0900

    supabaseの設定

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createClient } from "@supabase/supabase-js";
+import { SesstionContextProvider } from "@supabase/auth-helpers-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const supabase = createClient(
+  "https://pypqnlxfcqnbbacisnga.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5cHFubHhmY3FuYmJhY2lzbmdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk1MTE1MjMsImV4cCI6MjAxNTA4NzUyM30.SZzpt4tH0kifKEoV5eYh30pAosRdwo_tMxmwOHGmE-A"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SesstionContextProvider supabaseClient={supabase}>
+      <App />
+    </SesstionContextProvider>
   </React.StrictMode>
 );
 
