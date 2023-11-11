170bd150b3dc6049bad6ac03d8bcb2417d2ea6a7
Akito08 <sza9ki8to7@gmail.com>
2023-11-11 20:36:27 +0900

    eventの表示の仕方を変更

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,4 +1,51 @@
 export default function Event({ event, onToggleEvent }) {
+  function fixContestTime(startTime, endTime, category) {
+    const startDate = new Date(startTime);
+    const endDate = new Date(endTime);
+
+    const optionsDate = {
+      month: "2-digit",
+      day: "2-digit",
+      weekday: "short",
+    };
+
+    const optionsTime = {
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+
+    const formattedStartDate = new Intl.DateTimeFormat(
+      "ja-JP",
+      optionsDate
+    ).format(startDate);
+    const formattedStartTime = new Intl.DateTimeFormat(
+      "ja-JP",
+      optionsTime
+    ).format(startDate);
+
+    let formattedString = `${formattedStartDate} ${formattedStartTime}～`;
+
+    if (category === "AHC") {
+      const formattedEndDate = new Intl.DateTimeFormat(
+        "ja-JP",
+        optionsDate
+      ).format(endDate);
+      const formattedEndTime = new Intl.DateTimeFormat(
+        "ja-JP",
+        optionsTime
+      ).format(endDate);
+      formattedString += `${formattedEndDate}${formattedEndTime}`;
+    } else {
+      const formattedEndTime = new Intl.DateTimeFormat(
+        "ja-JP",
+        optionsTime
+      ).format(endDate);
+      formattedString += formattedEndTime;
+    }
+
+    return formattedString;
+  }
+
   return (
     <li>
       <input
@@ -6,6 +53,14 @@ export default function Event({ event, onToggleEvent }) {
         value={event.checked}
         onChange={() => onToggleEvent(event.id)}
       />
+      <span> </span>
+      <span>
+        {fixContestTime(
+          event.contest_start_time,
+          event.contest_end_time,
+          event.contest_category
+        )}
+      </span>
       <span> {event.contest_name}</span>
     </li>
   );
